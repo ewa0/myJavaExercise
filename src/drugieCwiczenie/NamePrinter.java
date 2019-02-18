18616613dcff851ee0caad0c8a91cb1d30cd4453
Ewa <ewus.ia00@gmail.com>
2019-02-18 20:20:46 +0100

    Name printer file created.

--- src/drugieCwiczenie/NamePrinter.java
+++ src/drugieCwiczenie/NamePrinter.java
@@ -1,4 +1,12 @@
 package drugieCwiczenie;
 
 public class NamePrinter {
+    public static void main(String[] args) {
+        System.out.println('a' + 'A');
+        System.out.println(1 + 2);
+        System.out.println(1.0 + 2.0);
+        System.out.println("cudzysłów \"");
+        System.out.println(true);
+    }
 }
+
