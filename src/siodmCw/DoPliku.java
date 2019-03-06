84cf2ce25e7dc2fc8876179bbef2f5e75cd43659
Ewa Sroczyk <es@nutopi.com>
2019-03-06 10:09:10 +0100

    Zapis i odczyt z pliku.

--- src/siodmCw/DoPliku.java
+++ src/siodmCw/DoPliku.java
@@ -1,4 +1,23 @@
 package siodmCw;
 
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
 public class DoPliku {
+    public static void main(String[] args) throws FileNotFoundException {
+        File plik = new File("plik.txt");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Podaj swoje imie: ");
+        String imie = input.nextLine();
+
+        PrintWriter zapis = new PrintWriter("plik.txt");
+        zapis.print(imie);
+        zapis.close();
+
+        Scanner odczyt = new Scanner(new File("plik.txt"));
+        System.out.println(odczyt.nextLine());
+    }
 }
+
