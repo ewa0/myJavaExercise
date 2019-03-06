2c24d0490bad339ab05b65558765d6984f99181b
Ewa Sroczyk <es@nutopi.com>
2019-03-06 10:08:48 +0100

    Porownywanie Stringow przy pomocy instrukcji if.

--- src/szosteCw/Imiona.java
+++ src/szosteCw/Imiona.java
@@ -1,4 +1,23 @@
 package szosteCw;
 
+import java.util.Scanner;
+
 public class Imiona {
+    public Imiona() {
+    }
+
+    public static void main(String[] args) {
+        Scanner odczyt = new Scanner(System.in);
+        System.out.println("Podaj imie: ");
+        String imie = odczyt.nextLine();
+        System.out.println("Podane imie to: " + imie);
+
+        if ("Ewa".equals(imie)) {
+            System.out.println("Czesc Ewa!");
+        } else if ("Maciek".equals(imie)) {
+            System.out.println("Czesc Maciek!");
+        } else {
+            System.out.println("Nie znam Cie.");
+        }
+    }
 }
