3660bc104c207b3180908365b2250cdb2cfd2580
Ewa Sroczyk <es@nutopi.com>
2019-03-07 19:34:35 +0100

    Wpisywanie imion przez uzytkownika do tablicy i wyswietlanie przy pomocy petli for.

--- src/dziewiateCw/ImieZTablicy.java
+++ src/dziewiateCw/ImieZTablicy.java
@@ -1,4 +1,22 @@
 package dziewiateCw;
 
+import java.util.Scanner;
+
 public class ImieZTablicy {
+    public static void main(String[] args) {
+        //tworzenie nowej tablicy
+        String[] tablica_imion = new String[5];
+
+        //wpisywanie imion do tablicy
+        for (int i = 0; i < tablica_imion.length; i++) {
+            Scanner odczyt = new Scanner(System.in);
+            System.out.println("Podaj imie nr " + i);
+            tablica_imion[i] = odczyt.nextLine();
+        }
+
+        //wyswietlanie imion z tablicy
+        for (int i = 0; i < tablica_imion.length; i++) {
+            System.out.println("Witaj " + tablica_imion[i] + "!");
+        }
+    }
 }
